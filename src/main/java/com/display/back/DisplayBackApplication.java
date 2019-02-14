a3c9c332bdcdffd836da5314d9b2f6d0b79f55a5
unknown <test@adami3.com>
2019-02-14 14:52:35 +0100

    Initial commit

---
+++ src/main/java/com/display/back/DisplayBackApplication.java
@@ -0,0 +1,14 @@
+package com.display.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisplayBackApplication.class, args);
+    }
+
+}
+
